e601702eb2044a862ebbf6b3b7623c98d5ea5440
Tom Monks <monksy@gmail.com>
2022-06-03 10:55:54 -0400

    Functionality to collapse and score complete rows

--- lib/tetris/game.ex
+++ lib/tetris/game.ex
@@ -48,6 +48,7 @@ defmodule Tetris.Game do
   def move_down_or_merge(game, old_tetro, _new, false = _valid) do
     game
     |> merge(old_tetro)
+    |> collapse_rows()
     |> new_tetromino()
     |> show()
     |> check_game_over()
@@ -61,6 +62,49 @@ defmodule Tetris.Game do
       |> Enum.into(game.junkyard)
 
     %{game | junkyard: new_junkyard}
+  end
+
+  def collapse_rows(game) do
+    rows = complete_rows(game)
+
+    game
+    |> absorb(rows)
+    |> score_rows(rows)
+  end
+
+  def absorb(game, []), do: game
+  def absorb(game, [y | ys]), do: remove_row(game, y) |> absorb(ys)
+
+  def remove_row(game, row) do
+    new_junkyard =
+      game.junkyard
+      |> Enum.reject(fn {{_x, y}, _shape} -> y == row end)
+      |> Enum.map(fn {{x, y}, shape} ->
+        {{x, maybe_move_y(y, row)}, shape}
+      end)
+      |> Map.new()
+
+    %{game | junkyard: new_junkyard}
+  end
+
+  defp maybe_move_y(y, row) when y < row, do: y + 1
+  defp maybe_move_y(y, _row), do: y
+
+  def score_rows(game, rows) do
+    new_score =
+      :math.pow(2, length(rows))
+      |> round()
+      |> Kernel.*(100)
+
+    %{game | score: new_score}
+  end
+
+  def complete_rows(game) do
+    game.junkyard
+    |> Map.keys()
+    |> Enum.group_by(&elem(&1, 1))
+    |> Enum.filter(fn {_y, list} -> length(list) == 10 end)
+    |> Enum.map(fn {y, _list} -> y end)
   end
 
   def junkyard_points(game) do
