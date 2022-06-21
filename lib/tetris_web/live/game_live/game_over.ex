1bf10367a065a8e9f32a0e249d0fd76d38dacc11
Tom Monks <monksy@gmail.com>
2022-06-21 11:08:54 -0400

    Functional new top score form

--- lib/tetris_web/live/game_live/game_over.ex
+++ lib/tetris_web/live/game_live/game_over.ex
@@ -2,11 +2,13 @@ defmodule TetrisWeb.GameLive.GameOver do
   use TetrisWeb, :live_view
 
   alias Tetris.Scores
+  alias Tetris.Scores.Score
 
   def mount(params, _session, socket) do
     {:ok,
      socket
      |> assign(score: params["score"])
+     |> assign(score_changeset: Scores.change_score(%Score{}))
      |> assign(top_scores: Scores.list_top_scores(10))}
   end
 
@@ -16,6 +18,13 @@ defmodule TetrisWeb.GameLive.GameOver do
         <div>
           <h1>Game Over!</h1>
           <h2>Your score: <%= @score %></h2>
+          <div>
+            <.form for={@score_changeset} let={f} phx-submit="save_score">
+              Enter your name: <%= text_input f, :player %>
+              <br />
+              <%= submit "Save" %>
+            </.form>
+          </div>
           <h2>Top scores:</h2>
           <%= for score <- @top_scores do %>
             <p><%= "#{score.player} - #{score.value}" %></p>
@@ -32,5 +41,15 @@ defmodule TetrisWeb.GameLive.GameOver do
 
   def handle_event("play", __data, socket) do
     {:noreply, play(socket)}
+  end
+
+  def handle_event("save_score", %{"score" => attrs}, socket) do
+    {:ok, _score} =
+      attrs
+      |> Map.put("value", socket.assigns.score)
+      |> Scores.create_score()
+
+    top_scores = Scores.list_top_scores(10)
+    {:noreply, assign(socket, :top_scores, top_scores)}
   end
 end
